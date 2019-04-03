df5a6d63efe941b0877ade17671a00fe75e3baa2
aretecode <aretecode@gmail.com>
2019-04-03 17:57:38 +0530

    📓 storybook init story

---
+++ stories/Example.story.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import App from '../src/App'
+
+storiesOf('example', module).add('default', () => (
+  <App>eh</App>
+))
