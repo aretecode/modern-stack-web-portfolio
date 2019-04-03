082b297465dfaa99935b42ecabe9611b98dad535
aretecode <aretecode@gmail.com>
2019-04-03 17:57:19 +0530

    🚨 test

---
+++ src/__tests__/example.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { render, fireEvent } from 'react-testing-library'
+import App from '../App'
+
+describe('example', () => {
+  it('simple react test', () => {
+    const handleSubmit = (serialized: {}) => {}
+    const mockSubmit = jest.fn(handleSubmit)
+
+    const { getByText, asFragment } = render(<App><button onClick={mockSubmit}>Continue</button></App>)
+    expect(mockSubmit.mock.calls.length).toEqual(0)
+
+    fireEvent.click(getByText(/Continue/))
+    expect(mockSubmit.mock.calls.length).toEqual(1)
+  })
+})
