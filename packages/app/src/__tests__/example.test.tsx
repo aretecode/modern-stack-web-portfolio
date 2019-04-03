73a1ae6fa5041666a80b024cb85a3638c70bde50
aretecode <aretecode@gmail.com>
2019-04-03 20:31:32 +0530

    📦  package structure

--- packages/app/src/__tests__/example.test.tsx
+++ packages/app/src/__tests__/example.test.tsx
@@ -4,10 +4,16 @@ import App from '../App'
 
 describe('example', () => {
   it('simple react test', () => {
-    const handleSubmit = (serialized: {}) => {}
+    const handleSubmit = (serialized: {}) => {
+      // empty
+    }
     const mockSubmit = jest.fn(handleSubmit)
 
-    const { getByText, asFragment } = render(<App><button onClick={mockSubmit}>Continue</button></App>)
+    const {getByText} = render(
+      <App>
+        <button onClick={mockSubmit}>Continue</button>
+      </App>
+    )
     expect(mockSubmit.mock.calls.length).toEqual(0)
 
     fireEvent.click(getByText(/Continue/))
