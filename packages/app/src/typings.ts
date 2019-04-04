8a407c7b8fe9509fa0e2bb4d1d4be4918b5fcac9
aretecode <aretecode@gmail.com>
2019-04-04 15:58:45 +0530

    ! add typings

---
+++ packages/app/src/typings.ts
@@ -0,0 +1,85 @@
+/**
+ * === basics ===
+ */
+export type Primitive = string | number | boolean | symbol | null | undefined | void
+
+export type SafePrimitive = string | number | boolean | symbol
+
+/**
+ * === serialized ===
+ */
+export interface SerializedObj {
+  [key: string]: Primitive | FrozenSerializedObj | FrozenSerializedArray
+  [key: number]: Primitive | FrozenSerializedObj | FrozenSerializedArray
+}
+
+export type FrozenSerializedObj = Readonly<SerializedObj>
+
+export interface FrozenSerializedArray extends Readonly<Serialized[]> {
+  readonly(x: number): Serialized
+}
+
+export type Serialized = SerializedObj | Primitive | FrozenSerializedObj | FrozenSerializedArray
+
+export type Real = AnyArrayOrObj | SafePrimitive
+
+/**
+ * === any ===
+ */
+
+export interface AnyObj extends Object {
+  [key: string]: any
+  [key: number]: any
+}
+
+export type AnyArray = any[]
+
+export type AnyArrayOrObj = (AnyObj & AnyArray) | AnyObj | AnyArray
+
+export type AnyFunction = (...args: any[]) => any
+
+/**
+ * === empty ===
+ */
+
+export type Empty = {[key: string]: never} | EmptyArray | '' | EmptySet | EmptyMap
+export interface EmptyMap<Key = string, Value = any> extends Map<Key, Value> {
+  size: 0
+}
+export interface EmptySet<Value = any> extends Set<Value> {
+  size: 0
+}
+export interface EmptyArray<Value = any> extends Array<Value> {
+  length: 0
+}
+
+/**
+ * === apollo ===
+ */
+import { ApolloCache } from 'apollo-cache'
+import { NormalizedCacheObject } from 'apollo-cache-inmemory'
+
+export type ResolverContext<ContextType extends object = {}> = {
+  [key: string]: unknown
+  cache: ApolloCache<NormalizedCacheObject>
+} & ContextType
+
+export type Resolver<
+  ArgsType extends object = AnyObj,
+  ContextType extends object = {},
+  ResponseType extends object = AnyObj
+> = (
+  obj: unknown,
+  args: ArgsType,
+  context: ResolverContext<ContextType>,
+  info: unknown
+) => ResponseType | Promise<ResponseType>
+
+export interface Resolvers<
+  ArgsType extends object = AnyObj,
+  ContextType extends object = {},
+  ResponseType extends object = AnyObj
+> {
+  Mutation?: {[key: string]: Resolver<ArgsType, ContextType, ResponseType>}
+  Query?: {[key: string]: Resolver<ArgsType, ContextType, ResponseType>}
+}
