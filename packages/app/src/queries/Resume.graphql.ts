362dcbdf1df2352dc37914c4a28d11e3b2edfe6d
aretecode <aretecode@gmail.com>
2019-04-05 16:16:31 +0530

    queries - add Resume query

---
+++ packages/app/src/queries/Resume.graphql.ts
@@ -0,0 +1,40 @@
+import { gql } from 'apollo-boost'
+
+export default gql`
+  query {
+    resume {
+      __typename
+      basics {
+        __typename
+        name
+        label
+        picture
+        email
+        telephone
+        website
+        summary
+        address
+        postalCode
+        city
+        countryCode
+        region
+        profiles {
+          __typename
+          network
+          username
+          url
+        }
+      }
+      work {
+        __typename
+        company
+        position
+        website
+        startDate
+        endDate
+        summary
+        highlights
+      }
+    }
+  }
+`
