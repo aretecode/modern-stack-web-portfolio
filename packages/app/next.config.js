e5e176531f99d6b94a5be0e8d00abe0b50512aba
aretecode <aretecode@gmail.com>
2019-04-09 04:26:30 +0530

    next.config - target serverless

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -2,6 +2,8 @@ const withTypescript = require('@zeit/next-typescript')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 /**
+ * @see https://zeit.co/examples/nextjs/
+ *
  * @todo add DefinePlugin
  * @example
  *   {
@@ -11,4 +13,6 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
  *     REALM_TYPE: 'browser' | 'node'
  *   }
  */
-module.exports = withTypescript()
+module.exports = withTypescript({
+  target: 'serverless',
+})
