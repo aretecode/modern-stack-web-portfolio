38db079bf519577c0e59746e42045ecdde92e467
aretecode <aretecode@gmail.com>
2019-04-08 16:41:05 +0530

    !! pages - about
    - styled elements
    - all using context
    
    - address component
    - skills component (wip)
    - make into a card
    - todo comments

---
+++ packages/app/src/pages/AboutPage/Address.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { ResumeContext, ResumeContextType } from '../../features/ResumeContext'
+
+export class Address extends React.PureComponent {
+  static contextType = ResumeContext
+  readonly context: ResumeContextType
+
+  render() {
+    const {
+      address,
+      postalCode,
+      city,
+      countryCode,
+      region,
+    } = this.context.basics
+
+    return (
+      <address>
+        {address}, {postalCode}, {city}, {region}, {countryCode}
+      </address>
+    )
+  }
+}
