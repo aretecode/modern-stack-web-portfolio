9a9c6535a3b0d73702d5e657259fb21d2723c319
aretecode <aretecode@gmail.com>
2019-04-08 16:42:40 +0530

    App - render all pages & styles

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,19 +1,47 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
 import ResumePage from './pages/ResumePage'
+import AboutPage from './pages/AboutPage'
+import Footer from './features/Footer'
+import Header from './features/Header'
 import AppWrap from './AppWrap'
+import { ResumeSchema } from './features/ResumeSchema'
+import { StyledVectorFilter } from './features/VectorFilter'
+import { AppStyles, BelowTheFoldStyles } from './AppStyles'
+/**
+ * currently am importing from here @@HACK
+ * as a way to load data and put it in ls
+ * before we have a UI for doing the mutation
+ *
+ * @example
+ *    import { resumeKeyValStore } from './storage'
+ *    resumeKeyValStore.set('resume', resume)
+ */
+// import './__resume'
+import './apolloStateRehydrate'
 
+/**
+ * @todo can also overlay the image as absolute with filter and opacity
+ */
 export default class App extends React.PureComponent {
-  public render() {
+  render() {
     return (
-      <AppWrap>
-        <Helmet>
-          <title>app</title>
-        </Helmet>
-        <h1>Started from the</h1>
-        {this.props.children}
-        <ResumePage />
-      </AppWrap>
+      <>
+        <AppStyles />
+        <AppWrap>
+          <Helmet>
+            <title>app</title>
+          </Helmet>
+          <Header />
+          <ResumeSchema />
+          {this.props.children}
+          <ResumePage />
+          <AboutPage />
+          <Footer />
+          <StyledVectorFilter />
+        </AppWrap>
+        <BelowTheFoldStyles />
+      </>
     )
   }
 }
