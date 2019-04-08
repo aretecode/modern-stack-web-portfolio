99a842b3bacd05c7f373928fa5d090ea36c459b5
aretecode <aretecode@gmail.com>
2019-04-08 14:44:21 +0530

    apolloState - implement mutation for setResume
    apolloState file:
    - move out default apolloState data and set default to empty
    - export pieces
    - add missing `picture` field to model
    - remove `addWork` from Mutation (_YAGNI_)
    - cleanup commented code
    - autofix
    - add typename to return of mutation

--- packages/app/src/apolloState.ts
+++ packages/app/src/apolloState.ts
@@ -8,7 +8,7 @@ import { Resolvers, ResumeType } from './typings'
 import { addTypeName } from './utils/addTypeName'
 import ResumeQuery from './queries/Resume.graphql'
 
-const typeDefs = gql`
+export const typeDefs = gql`
   type Profile {
     network: string
     username: string
@@ -32,11 +32,12 @@ const typeDefs = gql`
   type Work {
     company: string
     position: string
-    website: string
     startDate: string
     endDate: string
     summary: string
     highlights: [string]
+    website: string
+    picture: string
   }
   type Resume {
     id: ID
@@ -53,106 +54,92 @@ const typeDefs = gql`
   }
   type Mutation {
     setResume(basics: Basics, work: [Work]): AddOrUpdateResumeResponse!
-    addWork(id: ID, time: Work): AddOrUpdateResumeResponse!
   }
 `
 
-const apolloState = {
+export const apolloState = {
   defaults: {
+    // @todo WithTypeNameStrings<ResumeType>
     resume: {
       __typename: 'Resume',
       basics: {
         __typename: 'Basics',
-        name: 'eh',
-        label: '?',
-        picture: 'http://...',
-        email: 'email@teainahat.com',
-        telephone: '1250',
-        website: 'https://eh.com',
-        summary: 'summary',
-        profiles: [
-          {
-            __typename: 'Profile',
-            network: 'linkedin?',
-            username: 'aretecode',
-            url: 'https://www.linkedin.com/in/james-wiens/',
-          },
-        ],
-        address: 'address',
-        postalCode: 'v9j',
-        city: 'vancouver',
-        countryCode: 'CA',
-        region: 'BC',
+        name: '',
+        label: '',
+        picture: '',
+        email: '',
+        telephone: '',
+        website: '',
+        summary: '',
+        profiles: [],
+        address: '',
+        postalCode: '',
+        city: '',
+        countryCode: '',
+        region: '',
       },
-      work: [
-        {
-          __typename: 'Work',
-          company: 'Skava, an Infosys company',
-          position: 'senior lead technical architect',
-          website: 'https://skava.com',
-          startDate: '01/02/2017',
-          endDate: 'current',
-          summary: 'eh',
-          highlights: 'yay',
-        },
-      ],
-    },
+      work: [],
+    } as any,
   },
   resolvers: {
     Query: {
       async resume(obj, args, context, info) {
         logger.info('[query] resume')
 
-        const data = context.cache.read<{resume: ResumeType}>({
+        const data = context.cache.read<{ resume: ResumeType }>({
           query: ResumeQuery,
           optimistic: true,
         })
 
-        logger.log({data})
+        logger.log({ data })
 
-        return {...data!.resume}
-        // return {
-        //   __typename: 'Resume',
-        //   basics: {
-        //     __typename: 'Basics',
-        //     name: 'eh',
-        //   },
-        // }
+        return { ...data!.resume }
       },
     },
     Mutation: {
       /**
        * @todo generate typings from graphql schema & import
-       * @todo add schema for this
+       * could flatten it out
        */
-      async addResume(obj, args: {id?: string; basic: {}; profiles: unknown[]}, context, info) {
-        logger.info('[query] setTime')
+      async setResume(
+        obj,
+        args: { id?: string; basics: { profiles?: {} }; work: unknown[] },
+        context,
+        info
+      ) {
+        logger.info('[mutation] setResume')
         logger.dir(args)
 
         const updated = {
           __typename: 'Resume',
           basics: {
             __typename: 'Basics',
-            ...args.basic,
-            profiles: addTypeName('Profile', args.profiles),
+            ...args.basics,
+            profiles: addTypeName('Profile', args.basics.profiles),
           },
-          work: addTypeName('Work', args.profiles),
+          work: addTypeName('Work', args.work),
         }
+        logger.info('[mutation] updated')
+        logger.info(updated)
 
         context.cache.writeData({
-          data: updated,
+          data: {
+            resume: { ...updated },
+            ...updated,
+          },
         })
 
-        return {}
+        return {
+          __typename: 'AddOrUpdateResumeResponse',
+          responseMessage: 'Success if it does not throw?',
+        }
       },
     },
   } as Resolvers,
 }
 
-const stateLink = withClientState({
+export const stateLink = withClientState({
   cache,
   typeDefs,
   ...apolloState,
 })
-
-export { stateLink }
