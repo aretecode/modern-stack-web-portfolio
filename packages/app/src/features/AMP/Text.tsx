d9c6001a3dba38cc11a57ef10ee68df1161fef4b
aretecode <aretecode@gmail.com>
2019-04-08 16:45:34 +0530

    component / feature - init Text

---
+++ packages/app/src/features/AMP/Text.tsx
@@ -0,0 +1,21 @@
+/**
+ * @file Text
+ */
+import styled from 'styled-components'
+
+/**
+ * @alias color--emphasis
+ */
+export const StyledEmphasis = styled.span`
+  color: #ff4081;
+  text-shadow: 0 1px 0 rgba(255, 64, 129, 0.55), 0 2px 0 rgba(0, 0, 0, 0.1),
+    0 3px 0 rgba(0, 0, 0, 0.1), 0 4px 0 rgba(0, 0, 0, 0.1);
+`
+
+export const PageTitle = styled.h1`
+  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.55), 0 2px 0 rgba(0, 0, 0, 0.1),
+    0 3px 0 rgba(0, 0, 0, 0.1), 0 4px 0 rgba(0, 0, 0, 0.1);
+  font-size: 2em;
+  font-weight: normal;
+  font-weight: 300;
+`
