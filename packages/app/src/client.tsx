22e63d73d9c327eaf40d4883d4626b391e7562a4
aretecode <aretecode@gmail.com>
2019-04-09 06:02:15 +0530

    remove more unused razzle code

--- packages/app/src/client.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { hydrate } from 'react-dom'
-import App from './App'
-
-/**
- * @todo this file is only for browser, but we need provider on server as well
- * ^ so it is wrapping 2x
- */
-hydrate(<App />, document.getElementById('root') as HTMLElement)
