a1d36f5527de4a9c25d9694d35f0c45caa2ac615
aretecode <aretecode@gmail.com>
2019-04-11 03:17:37 +0530

    graphql - simplify implementation
    no need for update, delete, etc for now

--- packages/graphql/src/graphql-modules/resume/resolver.ts
+++ packages/graphql/src/graphql-modules/resume/resolver.ts
@@ -1,72 +1,27 @@
-import fetch from 'node-fetch'
-
-const resumeBaseUrl = `http://localhost:5555/resumes`
+import { readFileAsyncJson, writeFileAsyncJson, dbAbsolutePath } from '../../fs'
+import { logger } from '../../log'
 
 export default {
   Query: {
     resume: async (obj, args, context, info) => {
-      const { id } = args
-      const res = await fetch(`${resumeBaseUrl}/${id}`)
-      const response = await res.json()
-
-      console.log(response)
-
-      return response
-    },
-    resumes: async (obj, args, context, info) => {
-      const res = await fetch(`${resumeBaseUrl}`)
-      const response = await res.json()
-
-      console.log(response)
-      return response
+      try {
+        await writeFileAsyncJson(dbAbsolutePath, args)
+      } catch (fileSystemError) {
+        logger.error(fileSystemError)
+        /**
+         * @@security would want to hide the stack
+         */
+        throw fileSystemError
+      }
     },
   },
+  /**
+   * can have `deleteResume` & `updateResume`, but this is an example so KISS
+   */
   Mutation: {
-    createResume: async (obj, args, context, info) => {
-      const { userId, input } = args
-      const { title, completed } = input
-
-      const res = await fetch(resumeBaseUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          userId: userId ? userId : 0,
-          title,
-          completed: false,
-        }),
-      })
-
-      const response = await res.json()
-
+    setResume: async (obj, args, context, info) => {
+      const response = await readFileAsyncJson(dbAbsolutePath)
       return response
-    },
-    updateResume: async (obj, args, context, info) => {
-      const { id, input } = args
-
-      const res = await fetch(`${resumeBaseUrl}/${id}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          ...input,
-        }),
-      })
-
-      const response = await res.json()
-
-      return response
-    },
-    deleteResume: async (obj, args, context, info) => {
-      const { id } = args
-
-      const res = await fetch(`${resumeBaseUrl}/${id}`, {
-        method: 'DELETE',
-      })
-
-      return res.statusText
     },
   },
 }
--- packages/graphql/src/graphql-modules/resume/schema.ts
+++ packages/graphql/src/graphql-modules/resume/schema.ts
@@ -87,13 +87,9 @@ export default gql`
   }
 
   type Query {
-    resume(id: ID!): ResumeType
-    resumes: [ResumeType]
+    resume(id: ID): ResumeType
   }
   type Mutation {
-    createResume(userId: Int!, input: ResumeInputType!): ResumeType
-    updateResume(id: ID!, input: ResumeInputType!): ResumeType
-    toggleResume(id: ID!): ResumeType
-    deleteResume(id: ID!): String
+    setResume(basics: BasicsType, work: [WorkType]): AddOrUpdateResumeResponse
   }
 `
