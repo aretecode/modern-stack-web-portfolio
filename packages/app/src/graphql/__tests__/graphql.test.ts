13e09068464144f46474b3e6359ae5f1f98d4a54
aretecode <aretecode@gmail.com>
2019-04-12 01:39:09 +0530

    tests - graphql query syntax check

---
+++ packages/app/src/graphql/__tests__/graphql.test.ts
@@ -0,0 +1,8 @@
+import Resume from '../Resume'
+import SetResume from '../SetResume'
+
+describe('Graphql', () => {
+  it('should not have any syntax issues in the graphql files', () => {
+    expect(() => {}).not.toThrow()
+  })
+})
