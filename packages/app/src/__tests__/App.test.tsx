6115d1d7235323087a2b8f95ca9804bbee2e7019
aretecode <aretecode@gmail.com>
2019-04-12 01:45:57 +0530

    tests - App - add async waiting to render
    this will include data when it’s hooked up, for now, failing, since no cards are shown

--- packages/app/src/__tests__/App.test.tsx
+++ packages/app/src/__tests__/App.test.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
-import { render } from 'react-testing-library'
+import { render, waitForElement } from 'react-testing-library'
 import App from '../App'
 
+const sleep = async (time: number) => Promise.resolve(time)
+
 describe('app', () => {
-  it('should match snapshot', () => {
-    const { container } = render(<App />)
-    expect(container.innerHTML).toMatchSnapshot()
+  it('should match snapshot', async () => {
+    const view = <App />
+    const { container, getByTestId, rerender } = render(view)
+    rerender(view)
+    console.log('[tests] App - waiting')
+    await waitForElement(() => getByTestId('test-styled-card'))
+    await sleep(5000)
+
+    console.log('[tests] App - waited')
+    expect(container).toMatchSnapshot()
   })
 })
