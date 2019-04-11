038715c7b363e7bd5d3a8421efa9d9e4a24f606e
aretecode <aretecode@gmail.com>
2019-04-12 02:36:26 +0530

    only render GoogleDocument in amp

--- packages/app/src/features/GoogleDocument.tsx
+++ packages/app/src/features/GoogleDocument.tsx
@@ -1,31 +1,39 @@
 /**
- * could add non amp fallback
+ * @todo typings on `JSX.IntrinsicElements` in globals to extend the module for amp
  */
 import * as React from 'react'
 import Head from 'next/head'
 import { ResumeContext, ResumeContextType } from './ResumeContext'
+import { AmpContext } from './AmpContext'
 
 export default class GoogleDocument extends React.PureComponent {
   static contextType = ResumeContext
   readonly context: ResumeContextType
+
   render() {
     const { resumeWebsite } = this.context.basics
     return (
-      <>
-        <Head>
-          <script
-            async
-            custom-element="amp-google-document-embed"
-            src="https://cdn.ampproject.org/v0/amp-google-document-embed-0.1.js"
-          />
-        </Head>
-        <amp-google-document-embed
-          src={resumeWebsite}
-          width="8.5"
-          height="5"
-          layout="responsive"
-        />
-      </>
+      <AmpContext.Consumer>
+        {({ isAmp }) =>
+          isAmp === true && (
+            <>
+              <Head>
+                <script
+                  async
+                  custom-element="amp-google-document-embed"
+                  src="https://cdn.ampproject.org/v0/amp-google-document-embed-0.1.js"
+                />
+              </Head>
+              <amp-google-document-embed
+                src={resumeWebsite}
+                width="8.5"
+                height="5"
+                layout="responsive"
+              />
+            </>
+          )
+        }
+      </AmpContext.Consumer>
     )
   }
 }
